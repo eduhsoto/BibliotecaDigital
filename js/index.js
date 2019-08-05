b9cd0a80eeb7a94c59cd3903ed9763357d2e44fa
Eduardo <hseduardo98@gmail.com>
2019-08-05 00:36:03 -0500

    Mostrar
    Añadir funciones para mostrar datos.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 function iniciarSesionAdmin(){
   var user = document.getElementById("usuarioAdm").value;
   var pass = document.getElementById("passwordAdm").value;
@@ -19,4 +18,49 @@ function iniciarSesionAdmin(){
   });
   document.getElementById("registroAdmin").reset();
   return false;
+}
+
+function btnMostrar(){
+    $.ajax({
+        type:"get",
+        url: "http://localhost/crudbiblioteca/mostrarUsuarios.php",
+        dataType: "jsonp",
+        jsonp: "jsoncallback",
+        crossDomain: true,
+        success: function(data, status){
+          var options = '';
+          for (var i = 0; i < data.length; i++) {
+              options += '<option value=' + data[i].id_tipousuario + '>' + data[i].tipousuario + '</option>'
+          }
+          $('#selecti').html(options);
+            //document.getElementById("options").html(data);
+            /*select = document.createElement('select');
+            select.classList.add('form-control');
+            select.id = 'opciones';
+           $.each(data, function(i,item){
+             select.innerHTML += "<option value="+item.id_tipousuario+">"+item.tipousuario+"</option>"
+
+              });
+               document.getElementById("options").appendChild(select);*/
+                 }
+    });
+     return false;
+}
+
+function btnMostrarPersonal(){
+    $.ajax({
+        type:"get",
+        url: "http://localhost/crudbiblioteca/mostrarPersonal.php",
+        dataType: "jsonp",
+        jsonp: "jsoncallback",
+        crossDomain: true,
+        success: function(data, status){
+          var options = '';
+          for (var i = 0; i < data.length; i++) {
+              options += '<option value=' + data[i].id_tipousuario + '>'+ data[i].tipousuario + '</option>'
+          }
+          $('#tipopersonal').html(options);
+        }
+    });
+     return false;
 }
